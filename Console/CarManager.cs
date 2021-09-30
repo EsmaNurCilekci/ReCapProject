5a41e1386b4a43a723da0d48d66b4040edab5693
esmac <esmac@LAPTOP-3TRTK9OU>
2021-09-30 15:52:58 +0300

    Add project files.

---
+++ Console/CarManager.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Concrete.InMemory;
+
+namespace Console
+{
+    internal class CarManager
+    {
+        private InMemoryCarDal ınMemoryCarDal;
+
+        public CarManager(InMemoryCarDal ınMemoryCarDal)
+        {
+            this.ınMemoryCarDal = ınMemoryCarDal;
+        }
+    }
+}
